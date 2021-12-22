382365f2f1ece2dbba0d7092075aaa304ecf7b5a
avali4907 <avali4907@outlook.com>
2021-12-21 22:42:03 -0800

    started cache

--- finalproject.py
+++ finalproject.py
@@ -2,6 +2,7 @@ import sys
 
 from bs4 import BeautifulSoup
 from datetime import datetime
+import json
 from RecipeParsers import bingingwithbabish, cookieandkate, generalparse, gordonramsay, justonecookbook, maangchi, seriouseats, tasty, woksoflife
 import requests
 from urllib.parse import urlparse
@@ -23,24 +24,23 @@ def main(argvs):
     for url in argvs:
         ingredients = retrieve_ingredients(url)
         if ingredients:
-            grocery_list += retrieve_ingredients(url)
+            grocery_list.append(retrieve_ingredients(url))
             url_list.append(url)
         else:
             print(f'Could not find ingredients for {url}.')
     
-    filename = f'grocery_list_{datetime.now().strftime("%B-%d-%Y")}.txt'
-    
-    with open(filename, 'w', encoding='utf8') as f:
-        f.write('Grocery List\n')
-        f.write('---\n')
-        for item in grocery_list:
-            f.write(f'{item}\n')
-        f.write('\n')
-        f.write('Recipe Links\n')
-        f.write('---\n')
-        for url in url_list:
-            f.write(f'{url}\n')
-            
+    create_list(grocery_list, url_list)
+    is_correct = None
+    while(is_correct not in ['y', 'n']):
+        is_correct = input("does the list appears to be correct? (y/n)")
+    if is_correct == 'y':
+        dict = {}
+        for i, url in enumerate(url_list):
+                dict[url] = grocery_list[i]
+        with open('ingredient.json', 'w', encoding='utf8') as f:
+            f.write(json.dump(dict))
+
+
 
 def retrieve_ingredients(url):
     response = requests.get(url, headers={
@@ -58,6 +58,20 @@ def retrieve_ingredients(url):
         return ingredients
     else:
         return None
+
+def create_list(grocery_list, links):
+    filename = f'grocery_list_{datetime.now().strftime("%B-%d-%Y")}.txt'
+    
+    with open(filename, 'w', encoding='utf8') as f:
+        f.write('Grocery List\n')
+        f.write('---\n')
+        for item in grocery_list:
+            f.write(f'{item}\n')
+        f.write('\n')
+        f.write('Recipe Links\n')
+        f.write('---\n')
+        for url in links:
+            f.write(f'{url}\n')
 
 if __name__ == "__main__":
     # calling the main function
